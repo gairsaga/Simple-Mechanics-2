a907647484cbcb6966ba6fc5d5806c2609945545
sagar gaire <gairesagar90@gmail.com>
2017-09-07 19:01:45 -0600

    simple mechanics 2 done

---
+++ Simple Mechanics 2/Simple Mechanics2.cs
@@ -0,0 +1,129 @@
+﻿//Simple Mechanics.cs
+// CS-1181
+// Sagar Gaire
+// 9/7/2017
+// Anil Mandal
+// From this simple mechanics assignment we can learn how to use int,bool,double and string as well as other data type
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Mechanics_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int myNumber;
+            int anotherNumber;
+            int ThirdNumber;
+            int numnerOfsteps;
+            int myMomnumber;
+            int schoolNumber;
+            int principleNumber;
+            int gpaNumber;
+            int mobileNumber;
+            double averageScore;
+            double totalDistanceTraveled;
+             double minuteLeftinClass;
+            double testGrade;
+            double areaOfCircle;
+            double volumeOfCube;
+            double surfaceAreaOfCube;
+            double lateralAreaOfCone;
+            double volumeOfPyramid;
+            double areaOfRectengle; ;
+            double areaOfSquare;
+            string firstName;
+            string firstAndLastName;
+            string textFromFile;
+            string favoriteColor;
+            string aimInLife;
+            string futurePlan;
+            string yourGirlFriendName;
+            string yourSchoolName;
+            string yourFavouriteTeacher;
+            bool namedIsOdd;
+            bool namedIsEven;
+            bool namedWasLeftOver;
+            bool namedIsChecked;
+            bool namedIsAvailable;
+            bool namedIsNamed;
+            bool namedIsLeftOver;
+            bool namedIsNatural;
+            bool namedIsPlular;
+            bool namedIsSingular;
+            bool namedIsAddition;
+            bool namedIsDollor;
+            int aNumber = 10;
+            int giveMeANumber = 43;
+            int stepsTaken = 5346;
+            int numberOfEggs = 681;
+            int numberOfCar = 20;
+            int numberOfStudentInClass = 90;
+            int valueEntered = 30;
+            int whatIsYourPhoneNumber = 2087050359;
+            int timeTakenInMinute = 30;
+            int numberOfUniversityInaMERICA = 3000;
+            double changeLeft = 34.5;
+            double productPrice = 1567.252;
+            double temperatureRightNow = 96.0;
+            double circumferenceOfCircle = 50.11;
+            double timeLeft = 18.6426;
+            double volumeOfCircle = 30.2;
+            double araeOfHemesphere = 96.23;
+            double pizzaPriceInNepal = 9.99;
+            double ticketPrice = 16.99;
+            double marketPriceForWatch = 99.99;
+            double totalAreaOfPrism = 20.3;
+            string myText = "Hello";
+            string herTown = "San Francisco";
+            string actorName = "Mary Tyler Moore";
+            string theSaying = "Apple of my eye";
+            string favoriteColors = "Blue";
+            string myAimInLife = "Engineer";
+            string bestNepaliActor = "Dayang Rai";
+            string universityName = "ISU";
+            string bestTeacherInISU = "Anil";
+            string favouriteFruit = "Apple";
+            string bestFoothballPlayer = "Messi";
+            bool namedIsLeft = true;
+            bool namedIsRight = false;
+            bool namedIsNotUp = true;
+            bool namedIsToggled = false;
+            bool namedIsVisible = false;
+            bool namedkeepMoving = true;
+            bool namedWasStopped = false;
+            bool namedWasEven = true;
+            bool namedIsSagar = false;
+            bool namedIsInfinity = true;
+            bool namedWasDurable = false;
+            bool namedIscorrect = true;
+            bool namedWasGood = false;
+            int number1, number2;
+            int firstDay, secondDay,thirdDay;
+            double price1, price2;
+            string startingText, endingText;
+            bool oddRow, evenRow;
+            int countRed = 47, countBlue = 92;
+            int openBoxes = 153, returnedItems = 59;
+            double item1Quantity = 99.6, item2Quality = 86.7;
+            string openingPhrase = "Once upon a time", closingPhrase = "Happily ever after.";
+            bool isRich = false, isLicked = true;
+
+
+
+
+
+
+
+
+        }
+    }
+}
